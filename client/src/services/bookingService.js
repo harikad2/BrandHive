0b44b90b3e6f8e440739d1c21b149e18068da3cd
harikad2 <238w1a12d2@vrsec.ac.in>
2026-04-18 15:27:49 +0530

    booking feature

---
+++ client/src/services/bookingService.js
@@ -0,0 +1,14 @@
+import { getFirestore, collection, addDoc } from "firebase/firestore";
+import { app } from "../firebase/config";
+
+const db = getFirestore(app);
+
+export const addBooking = async (data) => {
+  try {
+    const docRef = await addDoc(collection(db, "bookings"), data);
+    console.log("Document written with ID:", docRef.id);
+  } catch (error) {
+    console.error("Error adding document:", error);
+    alert("Error saving booking");
+  }
+};
