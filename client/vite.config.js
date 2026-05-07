a762e0db9c7007f3bc10890c3d68be3462ea289b
harikad2 <238w1a12d2@vrsec.ac.in>
2026-05-07 20:02:19 +0530

    deploying updates

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,4 +5,16 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('firebase')) return 'firebase';
+            if (id.includes('react') || id.includes('react-dom') || id.includes('react-router')) return 'vendor';
+          }
+        }
+      }
+    }
+  }
 })
